5fc493477e43823c6e4f9d8644fb1978ad3fc1cc
Alexander <75177317+sasha523@users.noreply.github.com>
2022-10-07 19:25:37 +0300

    Create _form.php

---
+++ modules/admin/views/page/_form.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Форма для добавления и редактирования страницы, файл modules/admin/views/page/_form.php
+ */
+use app\modules\admin\models\Page;
+use mihaildev\ckeditor\CKEditor;
+use mihaildev\elfinder\ElFinder;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\admin\models\Page */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<?php $form = ActiveForm::begin(); ?>
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'slug')->textInput(['maxlength' => true]); ?>
+    <?php
+    // при редактировании существующей страницы нельзя допустить,
+    // чтобы в качестве родителя была выбрана эта же страница
+    $exclude = 0;
+    if (!empty($model->id)) {
+        $exclude = $model->id;
+    }
+    echo $form->field($model, 'parent_id')->dropDownList(Page::getRootPages($exclude));
+    ?>
+    <?php
+
+echo $form->field($model, 'content')->widget(CKEditor::className(), [
+   'editorOptions' => ElFinder::ckeditorOptions('elfinder',[/* Some CKEditor Options */]),   
+ ]);
+ ?>
+    <?= $form->field($model, 'keywords')->textarea(['rows' => 2, 'maxlength' => true]); ?>
+    <?= $form->field($model, 'description')->textarea(['rows' => 2, 'maxlength' => true]); ?>
+    <div class="form-group">
+        <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
+    </div>
+<?php ActiveForm::end(); ?>
