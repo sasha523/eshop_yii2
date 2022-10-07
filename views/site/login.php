bec09480aa749bc3e90bc9167b620b9028e43710
Alexander <75177317+sasha523@users.noreply.github.com>
2022-10-07 20:56:49 +0300

    Add files via upload

---
+++ views/site/login.php
@@ -0,0 +1,48 @@
+<?php
+
+/** @var yii\web\View $this */
+/** @var yii\bootstrap4\ActiveForm $form */
+/** @var app\models\LoginForm $model */
+
+use yii\bootstrap\ActiveForm;
+use yii\bootstrap\Html;
+
+$this->title = 'Вход';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="site-login container">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+  
+
+    <?php $form = ActiveForm::begin([
+        'id' => 'login-form',
+        'layout' => 'horizontal',
+        'fieldConfig' => [
+            'template' => "{label}\n<div  class=\"col-lg-5\">{input}</div>\n{error}", 
+            'labelOptions' => ['class' => 'col-lg-1 col-form-label mr-lg-3'],
+            'inputOptions' => ['class' => 'col-lg-3 form-control'],
+            'errorOptions' => ['class' => 'col-lg-7 invalid-feedback'],
+        ],
+    ]); ?>
+
+        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+
+        <?= $form->field($model, 'password')->passwordInput() ?>
+
+        <?= $form->field($model, 'rememberMe')->checkbox([
+            'template' => "<div class=\"offset-lg-1 col-lg-3 custom-control custom-checkbox\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
+        ]) ?>
+
+        <div class="form-group">
+            <div class="offset-lg-1 col-lg-11">
+                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            </div>
+        </div>
+
+    <?php ActiveForm::end(); ?>
+
+    
+</div>
+
