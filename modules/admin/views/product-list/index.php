40af85f54565dbba6248957a60fe0d1fcfcfcc4c
Alexander <75177317+sasha523@users.noreply.github.com>
2022-10-07 19:27:58 +0300

    Add files via upload

---
+++ modules/admin/views/product-list/index.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ListView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel app\modules\admin\models\ProductSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Products';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="product-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Product', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php Pjax::begin(); ?>
+    <?php echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= ListView::widget([
+        'dataProvider' => $dataProvider,
+        'itemOptions' => ['class' => 'item'],
+        'itemView' => function ($model, $key, $index, $widget) {
+            return Html::a(Html::encode($model->name), ['view', 'id' => $model->id]);
+        },
+    ]) ?>
+
+    <?php Pjax::end(); ?>
+
+</div>
