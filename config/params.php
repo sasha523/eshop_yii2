eed40ceda7828f2302254d09ee20774eccb04fda
Alexander <75177317+sasha523@users.noreply.github.com>
2022-10-07 19:01:29 +0300

    Add files via upload

---
+++ config/params.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'adminPassword' => 'qwerty123456',
+    'senderEmail' => 'noreply@example.com',
+    'senderName' => 'Example.com mailer',
+    /*
+     * Название магазина, например «Lamoda» или «WildBerries»
+     */
+    'shopName' => 'Интернет магазин профессиональной косметики от бренда Dior',
+    /*
+     * Значения по умолчанию для мета-тегов title, keywords и description
+     */
+    'defaultTitle' => 'Интернет магазин профессиональной косметики от бренда Dior',
+    'defaultKeywords' => 'Мода, Ароматы и Аксессуары',
+    'defaultDescription' => 'Интернет магазин профессиональной косметики от бренда Dior.Откройте для себя мир Christian Dior: Мода, Ароматы и Аксессуары',
+    /*
+     * Количество товаров на странице для постраничной навигации
+     */
+    'pageSize' => 6
+];
