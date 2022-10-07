d18b02f259ffe624eb4eae422cf9b48e70698459
Alexander <75177317+sasha523@users.noreply.github.com>
2022-10-07 18:59:23 +0300

    Add files via upload

---
+++ components/menu_tpl/iconmenu.php
@@ -0,0 +1,16 @@
+<li>
+    <a href="" class="collapsed">
+        <?= $category['name'] ?>
+        <?php if(isset($category['childs']) ): ?>
+            <span class="badge pull-right">
+                    <i class="fa fa-plus"></i>
+              </span>
+        <?php endif; ?>
+    </a>
+    <?php if( isset($category['childs']) ): ?>
+        <ul>
+            <?=$this->getMenuHtml($category['childs']) ?>
+        </ul>
+    <?php endif; ?>
+</li>
+
