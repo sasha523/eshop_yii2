91f4ab474b14f59aac0669d34dcc20ef9b7bb6d9
Alexander <75177317+sasha523@users.noreply.github.com>
2022-10-07 20:04:56 +0300

    Add files via upload

---
+++ views/cart/cart-modal.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\helpers\Url;
+
+?>
+
+<?php if(!empty($session['cart'])):?>
+    <div class="table-responsive">
+        <table class="table table-hover table-striped">
+   <thead>
+       <tr>
+<th>Фото</th>
+<th>Наименование</th>
+<th>Кол-во</th>
+<th>Цена</th>
+<th><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></th>
+</tr>
+</thead>
+<tbody>
+<?php foreach($session['cart'] as $id => $item):?>
+    <tr>
+    <td><?= \yii\helpers\Html::img($item['img'], ['alt' => $item['name'], 'height' => 50]) ?></td>
+    <td><?=$item['name']?></td>
+    <td><?=$item['qty']?></td>
+    <td><?=$item['price']?></td>
+    <td><span class="glyphicon glyphicon-remove text-danger del-item" arial-hidden="true" data-id="<?= $id?>"></span></td>
+                     </tr>
+    <?php endforeach;?>
+  <tr>
+<td colspan="4">Итого</td>
+<td><?= $session['cart.qty']?></td>
+</tr>
+<tr>
+<td colspan="4">На сумму:</td>
+<td><?= $session['cart.sum']?></td>
+</tr>
+</tbody>
+        </table>
+    </div>
+    <?php else:?>
+        <h3>Корзина пуста</h3>
+        <?php endif;?>
