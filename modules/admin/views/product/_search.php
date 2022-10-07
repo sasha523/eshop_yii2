74f53a4a3a065b67a1e154e8f4adfafc1d99cd35
Alexander <75177317+sasha523@users.noreply.github.com>
2022-10-07 19:27:01 +0300

    Add files via upload

---
+++ modules/admin/views/product/_search.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\admin\models\ProductSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="product-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+        'options' => [
+            'data-pjax' => 1
+        ],
+    ]); ?>
+
+    <?= $form->field($model, 'name') ?>
+
+    <?= $form->field($model, 'min_price') ?>
+
+    <?= $form->field($model, 'max_price') ?>
+
+    <?php // echo $form->field($model, 'price') ?>
+
+    <?php // echo $form->field($model, 'keywords') ?>
+
+    <?php // echo $form->field($model, 'description') ?>
+
+    <?php // echo $form->field($model, 'image') ?>
+
+    <?php // echo $form->field($model, 'hit') ?>
+
+    <?php // echo $form->field($model, 'new') ?>
+
+    <?php // echo $form->field($model, 'sale') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ modules/admin/views/product/index.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * Страница списка всех товаров, файл modules/admin/views/product/index.php
+ */
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Товары каталога';
+?>
+
+<h1><?= Html::encode($this->title) ?></h1>
+
+<p>
+    <?= Html::a('Добавить товар', ['create'], ['class' => 'btn btn-success']) ?>
+</p>
+
+<div class="col-xs-4">
+ <?php echo $this->render('_search', ['model' => $searchModel]); ?>
+</div>
+
+
+
+<?=
+GridView::widget([
+    'dataProvider' => $dataProvider,
+    'columns' => [
+        ['class' => 'yii\grid\SerialColumn'],
+        'name',
+        [
+            'attribute' => 'category_id',
+            'value' => function($data){
+                return $data->getCategoryName();
+            }
+        ],
+        [
+            'attribute' => 'brand_id',
+            'value' => function($data){
+                return $data->getBrandName();
+            }
+        ],
+        'price',
+        [
+            'attribute' => 'hit',
+            'value' => function($data) {
+                return $data->hit ? 'Да' : 'Нет';
+            }
+        ],
+        [
+            'attribute' => 'new',
+            'value' => function($data) {
+                return $data->new ? 'Да' : 'Нет';
+            }
+        ],
+        [
+            'attribute' => 'sale',
+            'value' => function($data) {
+                return $data->sale ? 'Да' : 'Нет';
+            }
+        ],
+        ['class' => 'yii\grid\ActionColumn'],
+    ],
+]);
+?>
+
